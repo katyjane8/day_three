b5e836e033f965efff37e8b03f3b22a68f4159c4
katyjane8 <katy.jane8@gmail.com>
2017-10-04 10:13:02 -0600

    iteration2 complete

--- bad_connection.rb
+++ bad_connection.rb
@@ -1,24 +1,31 @@
-ready_to_quit = false
-count = 0
-puts "HELLO, THIS IS A GROCERY STORE!"
+def start
+  @ready_to_quit = false
+  @count = 0
+  puts "HELLO, THIS IS A GROCERY STORE!"
+end
 
-input = gets.chomp
-until ready_to_quit
-  if input == ""
-    puts "HELLO?!"
-    input = gets.chomp
-  elsif input == input.downcase
-    puts "I AM HAVING A HARD TIME HEARING YOU."
-    input = gets.chomp
-  elsif input != "GOODBYE!"
-    puts "NO, THIS IS NOT A PET STORE"
-    input = gets.chomp
-  else input == "GOODBYE!"
-    count += 1
-    puts "ANYTHING ELSE I CAN HELP WITH?"
-    input = gets.chomp
-    input == "GOODBYE!" && count > 0
-    puts "THANK YOU FOR CALLING!"
-    ready_to_quit = true
+def take_call
+  input = gets.chomp
+  until @ready_to_quit == true
+    if input == ""
+      puts "HELLO?!"
+      input = gets.chomp
+    elsif input == input.downcase
+      puts "I AM HAVING A HARD TIME HEARING YOU."
+      input = gets.chomp
+    elsif input != "GOODBYE!"
+      puts "NO, THIS IS NOT A PET STORE"
+      input = gets.chomp
+    else input == "GOODBYE!"
+      @count += 1
+      puts "ANYTHING ELSE I CAN HELP WITH?"
+      input = gets.chomp
+      input == "GOODBYE!" && @count > 0
+      puts "THANK YOU FOR CALLING!"
+      @ready_to_quit = true
+    end
   end
 end
+
+start
+take_call
